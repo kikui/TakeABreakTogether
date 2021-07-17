d55bd83ce7db5bcfefcdc95ee4de6cb79bc5cb30
athery <athery@snapp.fr>
2021-07-17 03:15:39 +0200

    survey stat OK

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -26,4 +26,30 @@ class Survey < ApplicationRecord
     selected_proposal.nil? ? "-" : selected_proposal.name
   end
 
+  def most_dificultless 
+    winner = nil
+    count = 0
+    proposal_ids = self.proposals.map{ |p| p.id }
+    self.group.user_groups.each do |ug|
+      new_count = Vote.where(user_id: ug.user_id, proposal_id: proposal_ids).count
+      winner = ug.user if new_count > count
+      count = new_count
+    end
+    winner.nil? ? "-" : winner.pseudo
+  end
+
+  def most_too_late 
+    proposal_ids = self.proposals.map { |p| p.id }
+    user_ids = self.group.user_groups.map { |ug| ug.user_id }
+    vote = Vote.where(user_id: user_ids, proposal_id: proposal_ids).sort_by{ |vote| vote.created_at }.reverse!.first
+    vote.nil? ? "-" : vote.user.pseudo
+  end
+
+  def user_no_vote
+    user_ids = self.group.user_groups.map { |ug| ug.user_id }
+    user_ids_voted = Vote.where(user_id: user_ids, proposal_id: self.proposals.map { |p| p.id }).map { |vote| vote.user_id }.uniq
+    list = user_ids - user_ids_voted
+    list.length > 0 ? User.where(id: list).map{ |user| user.pseudo } : ["-"]
+  end
+
 end
